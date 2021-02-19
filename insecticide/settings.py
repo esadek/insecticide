4aac097642f043a0fb72ac4de74d854814813838
Emil Sadek <esadek@hotmail.com>
2021-02-19 12:54:54 -0800

    Change: database from SQLite to PostgreSQL

--- insecticide/settings.py
+++ insecticide/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'insecticide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insecticide',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
