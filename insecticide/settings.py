a8dcbfc2aac3ccca4917029675e601d38b730a55
Emil Sadek <esadek@hotmail.com>
2021-03-03 18:04:06 -0800

    Remove: bugs app

--- insecticide/settings.py
+++ insecticide/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'whu)2fh=4cjp_ii+e24+s4lqe1c0dl$amox4-6-j-49d&$%4vb'
+SECRET_KEY = '1g6gz@w*2z!1r()#i2agzzbq52j8soa=c&s-vlo8)+xca-h651'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'insecticide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'insecticide',
-        'USER': 'username',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
