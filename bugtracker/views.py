afaf2e2c5a7a0b4bfa6563b1ad799b5f97b6f9c8
Emil Sadek <esadek@hotmail.com>
2021-03-30 21:52:32 -0700

    Add: login url

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
--- insecticide/urls.py
+++ insecticide/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('bugtracker.urls')),
     path('admin/', admin.site.urls),
+    path('login/', include('bugtracker.urls'))
 ]
