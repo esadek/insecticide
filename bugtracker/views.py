9e56ad810de39f4c1bcb5a96306f06fdaac63cba
Emil Sadek <esadek@hotmail.com>
2021-04-01 18:32:35 -0700

    Change: root view

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
 def login(request):
     return render(request, 'login.html')
