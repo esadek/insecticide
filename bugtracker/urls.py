c0532895c62ee4af6ed05d373f64a0702aedb6b3
Emil Sadek <esadek@hotmail.com>
2021-04-13 21:20:58 -0700

    Add: projects url

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('projects/', views.projects, name='projects'),
     path('bugs/', views.bugs, name='bugs')
 ]
