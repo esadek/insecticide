12e03e7c3877c17fc65721e45eced81c80eb1393
Emil Sadek <esadek@hotmail.com>
2021-04-14 15:31:31 -0700

    Add: create project and bug views

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from . import models
+from . import models, forms
 
 
 def index(request):
@@ -25,6 +25,17 @@ def project(request, id):
     context = {'project': project}
     return render(request, 'project.html', context)
 
+def create_project(request):
+    if request.method == 'POST':
+        form = forms.ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    else:
+        form = forms.ProjectForm()
+    context = {'form': form}
+    return render(request, 'form.html', context)
+
 def bugs(request):
     bugs = models.Bug.objects.all()
     context = {'bugs': bugs}
@@ -34,3 +45,14 @@ def bug(request, id):
     bug = models.Bug.objects.get(id=id)
     context = {'bug': bug}
     return render(request, 'bug.html', context)
+
+def create_bug(request):
+    if request.method == 'POST':
+        form = forms.BugForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bugs')
+    else:
+        form = forms.BugForm()
+    context = {'form': form}
+    return render(request, 'form.html', context)
