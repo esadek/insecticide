3a63c37a3850a434eeb8890a5384d5293b360ad4
Emil Sadek <esadek@hotmail.com>
2021-04-14 11:24:44 -0700

    Add: dashboard url

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('projects/', views.projects, name='projects'),
     path('project/<int:id>/', views.project, name='project'),
     path('bugs/', views.bugs, name='bugs'),
