60565cb3395b0b56f9e9615ab28636467bb509b1
Emil Sadek <esadek@hotmail.com>
2021-04-14 15:31:01 -0700

    Add: create project and bug urls

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('projects/', views.projects, name='projects'),
+    path('projects/create/', views.create_project, name='create project'),
     path('project/<int:id>/', views.project, name='project'),
     path('bugs/', views.bugs, name='bugs'),
-    path('bug/<int:id>/', views.bug, name='bug')
+    path('bugs/create/', views.create_bug, name='create bug'),
+    path('bug/<int:id>/', views.bug, name='bug'),
 ]
