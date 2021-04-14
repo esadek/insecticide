5544af5d8d186e1d858ddc2df35f49c9954ace69
Emil Sadek <esadek@hotmail.com>
2021-04-13 18:23:15 -0700

    Add: project model

--- bugtracker/admin.py
+++ bugtracker/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Bug
+from .models import Project, Bug
 
+admin.site.register(Project)
 admin.site.register(Bug)
--- bugtracker/models.py
+++ bugtracker/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+
 class Bug(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
     creation_date = models.DateField()
     due_date = models.DateField()
