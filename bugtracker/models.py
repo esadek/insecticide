71a211facb902ea85003f604aa8c9beba43ea334
Emil Sadek <esadek@hotmail.com>
2021-04-13 19:10:14 -0700

    Add: bug status and priority

--- bugtracker/models.py
+++ bugtracker/models.py
@@ -10,9 +10,23 @@ class Project(models.Model):
 
 
 class Bug(models.Model):
+    STATUS = [
+        ('Open', 'Open'),
+        ('In progress', 'In progress'),
+        ('To be tested', 'To be tested'),
+        ('Reopen', 'Reopen'),
+        ('Closed', 'Closed')
+    ]
+    PRIORITY = [
+        ('High', 'High'),
+        ('Normal', 'Normal'),
+        ('Low', 'Low')
+    ]
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
     project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS, default='Open')
+    priority = models.CharField(max_length=10, choices=PRIORITY, default='Normal')
     creation_date = models.DateField()
     due_date = models.DateField()
 
