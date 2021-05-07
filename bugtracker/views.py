8144f25799d20139d44603f6af4f7630029050da
Emil Sadek <esadek@hotmail.com>
2021-05-06 21:47:25 -0700

    Add: blank lines

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -6,21 +6,26 @@ from . import models, forms
 def index(request):
     return render(request, 'index.html')
 
+
 def signup(request):
     return render(request, 'signup.html')
 
+
 def dashboard(request):
     return render(request, 'dashboard.html')
+
 
 def projects(request):
     projects = models.Project.objects.all()
     context = {'projects': projects}
     return render(request, 'projects.html', context)
 
+
 def project(request, id):
     project = models.Project.objects.get(id=id)
     context = {'project': project}
     return render(request, 'project.html', context)
+
 
 def create_project(request):
     if request.method == 'POST':
@@ -33,15 +38,18 @@ def create_project(request):
     context = {'form': form}
     return render(request, 'form.html', context)
 
+
 def bugs(request):
     bugs = models.Bug.objects.all()
     context = {'bugs': bugs}
     return render(request, 'bugs.html', context)
 
+
 def bug(request, id):
     bug = models.Bug.objects.get(id=id)
     context = {'bug': bug}
     return render(request, 'bug.html', context)
+
 
 def create_bug(request):
     if request.method == 'POST':
