d2e75c395f396fff937caaf3bf3ded040e63ef2e
Emil Sadek <esadek@hotmail.com>
2021-05-06 21:02:23 -0700

    Remove: login path

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('projects/', views.projects, name='projects'),
