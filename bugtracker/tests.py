9c60140e5223530415fe015f2c42379445e40a28
Emil Sadek <esadek@hotmail.com>
2021-05-19 20:30:05 -0700

    Add: project model test

--- bugtracker/tests.py
+++ bugtracker/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+
+class ProjectTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.project = models.Project(
+            name = 'test name',
+            description = 'test description'
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.project), self.project.name)
