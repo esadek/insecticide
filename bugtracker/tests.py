51ec4120b5fc834675e3a9a2a9358f2e06a8ee22
Emil Sadek <esadek@hotmail.com>
2021-05-28 18:01:53 -0700

    Add: bug model test

--- bugtracker/tests.py
+++ bugtracker/tests.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.test import TestCase
 from . import models
 
@@ -13,3 +14,16 @@ class ProjectTestCase(TestCase):
 
     def test_str(self):
         self.assertEqual(str(self.project), self.project.name)
+
+
+class BugTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.bug = models.Bug(
+            name = 'test name',
+            description = 'test description',
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.bug), self.bug.name)
