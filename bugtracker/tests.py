209ba0359c2305090e345a090feb26cfacd1a0b6
Emil Sadek <esadek@hotmail.com>
2021-08-10 16:49:18 -0700

    Remove: tests

--- bugtracker/tests.py
+++ bugtracker/tests.py
@@ -1,29 +1,2 @@
-from datetime import date
 from django.test import TestCase
 from . import models
-
-
-class ProjectTestCase(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.project = models.Project(
-            name = 'test name',
-            description = 'test description'
-        )
-
-    def test_str(self):
-        self.assertEqual(str(self.project), self.project.name)
-
-
-class BugTestCase(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.bug = models.Bug(
-            name = 'test name',
-            description = 'test description',
-        )
-
-    def test_str(self):
-        self.assertEqual(str(self.bug), self.bug.name)
